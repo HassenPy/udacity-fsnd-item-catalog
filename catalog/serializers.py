3d39dc76a42a0d30a998ff3b8ed98563af8e0cd7
Hassen ben tanfous <hbt.youtube@yahoo.com>
2017-07-31 23:52:00 +0000

    add category list serializer

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,5 +1,5 @@
 """JSON serializers for the catalog app."""
-# from flask import url_for
+from app.settings import Config
 
 
 class ItemSerializer(object):
@@ -22,7 +22,7 @@ class ItemSerializer(object):
         }
 
 
-class CategorySerializer(object):
+class CategoryPageSerializer(object):
     """Minimal paginator to dict serializer."""
 
     def __init__(self, title, description, paginator):
@@ -46,3 +46,23 @@ class CategorySerializer(object):
             'next_page': self.next_page,
             'prev_page': self.prev_page
         }
+
+
+class CategoryListSerializer(object):
+    """Minimal paginator to dict serializer."""
+
+    def __init__(self, categories):
+        """PaginationSerializer constructor."""
+        self.categories = categories
+
+    def serialize(self):
+        """Return dict representation of paginator."""
+        categories_serialized = []
+        for category in self.categories:
+            categories_serialized.append({
+                'title': category.title,
+                'location': '%s/catalog/category/%d' %
+                                        (Config.domain, category.id)
+            })
+        print(categories_serialized)
+        return categories_serialized
