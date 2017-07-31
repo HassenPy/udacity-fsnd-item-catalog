7f90546e8cba61ce6ebfbb756cd4e43b8f15d4cd
Hassen ben tanfous <hbt.youtube@yahoo.com>
2017-07-31 18:23:33 +0000

    add Category and Item model validators

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,10 @@
 """Catalog app models."""
+import bleach
+from validators import url
+from validators.utils import ValidationFailure
+
+from slugify import slugify
+
 from app import db
 
 
@@ -8,16 +14,41 @@ class Category(db.Model):
     __tablename__ = 'category'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(20), unique=True)
+    title = db.Column(db.String(25), unique=True)
+    slug = db.Column(db.String(25), unique=True)
     description = db.Column(db.Text())
 
     def __init__(self, title, description):
         """Class constructor."""
-        self.title = title
-        self.description = description
+        self.title = bleach.clean(title)
+        self.description = bleach.clean(description)
+        self.slug = slugify(self.title)
         self.errors = {}
         self.validators = [
+            self.validate_title,
         ]
+
+    def validate_title(self):
+        """Title validator."""
+        field = self.title
+        errors = []
+
+        if not (len(field) in range(3, 25)):
+            errors.append(
+                    "Title must be longer than 3 and "
+                    "shorter than 25 characters"
+            )
+
+        if errors:
+            self.errors["title"] = errors
+
+    def is_valid(self):
+        """Check if the values passed to the model are valid."""
+        for validator in self.validators:
+            validator()
+        if self.errors:
+            return False
+        return True
 
     def __unicode__(self):
         """Text representation of the Category class instance."""
@@ -34,20 +65,59 @@ class Item(db.Model):
     __tablename__ = 'item'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(20), unique=True)
-    link = db.Column(db.String(20))
+    title = db.Column(db.String(35), unique=True)
+    link = db.Column(db.String(1000))
     author = db.Column(db.Integer, db.ForeignKey('user.id'))
     category = db.Column(db.Integer, db.ForeignKey('category.id'))
 
     def __init__(self, title, link, author, category):
         """Class constructor."""
-        self.title = title
-        self.link = link
+        self.title = bleach.clean(title)
+        self.link = bleach.linkify(link)
         self.author = author
         self.category = category
         self.errors = {}
         self.validators = [
+            self.validate_title,
+            self.validate_link
         ]
+
+    def validate_title(self):
+        """Title validator."""
+        field = self.title
+        errors = []
+
+        if not (len(field) in range(5, 25)):
+            errors.append(
+                    "Title must be longer than 5 and "
+                    "shorter than 25 characters"
+            )
+
+        if errors:
+            self.errors["title"] = errors
+
+    def validate_link(self):
+        """Validate link using the python validators library."""
+        field = self.link
+        errors = []
+
+        # kind of weird way of raising a validation error.
+        if isinstance(url(field, public=True), ValidationFailure):
+            errors.append(
+                    "Title must be longer than 5 and "
+                    "shorter than 25 characters"
+            )
+
+        if errors:
+            self.errors["link"] = errors
+
+    def is_valid(self):
+        """Check if the values passed to the model are valid."""
+        for validator in self.validators:
+            validator()
+        if self.errors:
+            return False
+        return True
 
     def __unicode__(self):
         """Text representation of the Item class instance."""
