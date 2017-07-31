dc9a21d9bc956069d894b9e522b5f6df3e3d8519
Hassen ben tanfous <hbt.youtube@yahoo.com>
2017-07-31 01:19:27 +0000

    fix user model table name

--- auth/models.py
+++ auth/models.py
@@ -13,7 +13,7 @@ from app import db
 class User(db.Model, UserMixin):
     """A fat model that handles both the db and login methods."""
 
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(12), unique=True)
