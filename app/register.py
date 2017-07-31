ffbb6200969d6333e5ecb20251c05134959f841e
Hassen ben tanfous <hbt.youtube@Yahoo.com>
2017-07-31 14:11:39 +0100

    add catalog app to app subsciption logic

--- app/register.py
+++ app/register.py
@@ -4,4 +4,6 @@
 def register_apps(flasko):
     """Subscribe apps to the flask instance."""
     from auth.views import authApp
+    from catalog.views import catalogApp
     flasko.register_blueprint(authApp)
+    flasko.register_blueprint(catalogApp)
