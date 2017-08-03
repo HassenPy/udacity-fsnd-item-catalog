e5e94e1ba19112575c9d0638f8b2d2dd43c34d4a
Hassen ben tanfous <hbt.youtube@yahoo.com>
2017-08-03 01:43:47 +0000

    add item API tests

--- catalog/tests.py
+++ catalog/tests.py
@@ -201,6 +201,163 @@ class authTest(unittest.TestCase):
             response = c.delete('/catalog/category/1/')
             assert(response.status_code == 404)
 
+    def test_item_list_without_id(self):
+        """Check if endpoint returns a list of item when no id provided."""
+        response = self.app.get("/catalog/item/")
+        # use bootstrap script data
+        assert(b"admin item" in response.data)
+        assert(b"user item" in response.data)
+        assert(response.status_code == 200)
+
+    def test_item_with_id(self):
+        """Check if endpoint returns the right item."""
+        response = self.app.get("/catalog/item/2/")
+        # use bootstrap script data
+        assert(b"user item" in response.data)
+        assert(b"https://www.youtube.com" in response.data)
+        assert(response.status_code == 200)
+
+    def test_item_post_csrf_protection(self):
+        """Check if endpoint is csrf protected."""
+        response = self.app.post('/catalog/item/')
+        assert(response.status_code == 403)
+
+    def test_item_unauthenticated_post(self):
+        """Check if endpoint is login only."""
+        with self.app as c:
+            token = c.get('/csrf-token')
+            token = json.loads(token.data.decode())['_csrf_token']
+            response = c.post('/catalog/item/', data={
+                '_csrf_token': token
+            })
+            assert(response.status_code == 401)
+
+    def test_item_post_data(self):
+        """Check endpoint data validation."""
+        with self.app as c:
+            token = c.get('/csrf-token')
+            token = json.loads(token.data.decode())['_csrf_token']
+            c.post('/login', data={
+                'username': 'admin',
+                'password': '12345678',
+                '_csrf_token': token
+            })
+
+            # Check for required params
+            token = c.get('/csrf-token')
+            token = json.loads(token.data.decode())['_csrf_token']
+            response = c.post('/catalog/item/', data={
+                'title': 'yo',
+                '_csrf_token': token
+            })
+            assert(response.status_code == 400)
+
+            # Check for erronous params
+            token = c.get('/csrf-token')
+            token = json.loads(token.data.decode())['_csrf_token']
+            response = c.post('/catalog/item/', data={
+                'title': 'yo',
+                '_csrf_token': token
+            })
+            assert(response.status_code == 400)
+
+            # Check for existing category
+            token = c.get('/csrf-token')
+            token = json.loads(token.data.decode())['_csrf_token']
+            response = c.post('/catalog/item/', data={
+                'title': 'admin item',
+                'link': 'http://www.google.com',
+                'category': 1,
+                '_csrf_token': token
+            })
+            assert(response.status_code == 409)
+
+    def test_item_put_user_priviliege(self):
+        """Check if endpoint rejects non owner updates."""
+        with self.app as c:
+            token = c.get('/csrf-token')
+            token = json.loads(token.data.decode())['_csrf_token']
+            c.post('/login', data={
+                'username': 'user',
+                'password': '12345678',
+                '_csrf_token': token
+            })
+
+            response = c.put('/catalog/item/1/', data={
+                'title': 'Oh i changed :/'
+            })
+            assert(response.status_code == 401)
+
+    def test_item_put_data(self):
+        """Check endpoint data validation."""
+        with self.app as c:
+            token = c.get('/csrf-token')
+            token = json.loads(token.data.decode())['_csrf_token']
+            c.post('/login', data={
+                'username': 'admin',
+                'password': '12345678',
+                '_csrf_token': token
+            })
+
+            # Missing required parameter
+            response = c.put('/catalog/item/1/')
+            assert(response.status_code == 400)
+
+            # Check for erronous params
+            response = c.put('/catalog/item/1/', data={
+                'title': 'yo',
+            })
+            assert(response.status_code == 400)
+
+            # Check for non existing category
+            response = c.put('/catalog/item/999/')
+            assert(response.status_code == 404)
+
+    def test_item_delete_priviliege(self):
+        """Test delete owner priviliege check."""
+        with self.app as c:
+            token = c.get('/csrf-token')
+            token = json.loads(token.data.decode())['_csrf_token']
+            c.post('/login', data={
+                'username': 'user',
+                'password': '12345678',
+                '_csrf_token': token
+            })
+
+            response = c.delete('/catalog/item/1/')
+            assert(response.status_code == 401)
+
+    def test_item_delete_non_existant_record(self):
+        """Test deleting a non existant record."""
+        with self.app as c:
+            token = c.get('/csrf-token')
+            token = json.loads(token.data.decode())['_csrf_token']
+            c.post('/login', data={
+                'username': 'user',
+                'password': '12345678',
+                '_csrf_token': token
+            })
+
+            response = c.delete('/catalog/item/99/')
+            assert(response.status_code == 404)
+
+    def test_item_delete_existant_record(self):
+        """Test deleting an existant record."""
+        with self.app as c:
+            token = c.get('/csrf-token')
+            token = json.loads(token.data.decode())['_csrf_token']
+            c.post('/login', data={
+                'username': 'admin',
+                'password': '12345678',
+                '_csrf_token': token
+            })
+
+            response = c.delete('/catalog/item/1/')
+            assert(response.status_code == 200)
+
+            response = c.delete('/catalog/item/1/')
+            assert(response.status_code == 404)
+
 
 if __name__ == "__main__":
     unittest.main()
