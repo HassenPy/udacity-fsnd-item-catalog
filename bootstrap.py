5cc433a30a63e1085392911f801622993a745eec
Hassen ben tanfous <hbt.youtube@yahoo.com>
2017-08-03 20:22:45 +0000

    add more realistic data

--- bootstrap.py
+++ bootstrap.py
@@ -33,26 +33,52 @@ def bootstrap(app, db):
     db.session.add(category)
     db.session.commit()
 
-    item = Item(title="admin item", link="http://www.google.com",
-                author=2, category=1)
+    item = Item(
+        title="Principles Of Minimalist Web Design, With Examples",
+        link=("https://www.smashingmagazine.com/2010/05/principles-of-"
+              "minimalist-web-design-with-examples/"),
+        author=2, category=1
+    )
     db.session.add(item)
-    item1 = Item(title="user item", link="https://www.youtube.com",
-                 author=1, category=1)
+    item1 = Item(
+        title="Testing Flask Apps, Something that is untested is broken",
+        link="http://flask.pocoo.org/docs/0.12/testing/",
+        author=1, category=1
+    )
     db.session.add(item1)
-    item2 = Item(title="facebook", link="https://www.facebook.com",
-                 author=2, category=2)
+    item2 = Item(
+        title="Flask Application Factories",
+        link="http://flask.pocoo.org/docs/0.12/patterns/appfactories/",
+        author=2, category=1
+    )
     db.session.add(item2)
-    item3 = Item(title="twitter", link="https://www.twitter.com",
-                 author=1, category=2)
+    item3 = Item(
+        title="5 things designers wish their clients from hell knew",
+        link=("https://medium.com/@KeiraBui/5-things-designers-wish-their"
+              "-clients-from-hell-knew-dac7a4126a53"),
+        author=1, category=2
+    )
     db.session.add(item3)
-    item4 = Item(title="reddit", link="https://www.reddit.com",
-                 author=2, category=1)
+    item4 = Item(
+        title=("Discover fundamentals of computer programming by playing a "
+               "board game!"),
+        link="http://www.c-jump.com/",
+        author=2, category=2
+    )
     db.session.add(item4)
-    item5 = Item(title="gmail", link="https://www.gmail.com",
-                 author=1, category=1)
+    item5 = Item(
+        title="How to Scale Django: Beyond the Basics",
+        link=("https://www.digitalocean.com/community/tutorials/how-to-"
+              "scale-django-beyond-the-basics"),
+        author=1, category=1)
     db.session.add(item5)
-    item6 = Item(title="vimeo", link="https://www.vimeo.com",
-                 author=2, category=1)
+    item6 = Item(
+        title="Where To Start When You’ve Procrastinated On "
+              "Your Goals For Too Long",
+        link=("https://medium.com/personal-growth/where-to-start-when-youve"
+              "-procrastinated-on-your-goals-for-too-long-ead87d0c91ba"),
+        author=2, category=1
+    )
     db.session.add(item6)
 
     db.session.commit()
