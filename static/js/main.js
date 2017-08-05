0d936cf14b12539e0273c0c9e8d1433488817847
Hassen ben tanfous <hbt.youtube@yahoo.com>
2017-08-05 11:01:09 +0000

    update static files \__(-.-)'_/

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,32 @@
 $(function(){
+  $("time.timeago").timeago();
+  var authors = $(".author");
+  authors.each(function(author){
+    elm = $(authors[author]);
+    url = elm.data('author');
+    $.ajax({
+        method: "GET",
+        url: url,
+        context: elm,
+        success: function(response){
+          var username = response['username'];
+          this.html('by ' + username);
+        }
+      });
+    });
+
+    $('#deleteModal').on('shown.bs.modal', function (e) {
+      e.preventDefault();
+    });
+    $('.delete').click(function(e){
+      $('#deleteButton').data('action', $(this).data('href'));
+      e.preventDefault();
+    });
+    $('#deleteButton').click(function(e){
+      $.ajax({
+        method: "DELETE",
+        url: $(this).data('action')
+      });
+    });
 
 });
